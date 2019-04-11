1aa893f2701d905dc327b7e2deb78fa57cfa908c
Pedro Gabriel <gaboleal123@gmail.com>
2019-04-10 23:55:34 -0430

    Maquetado
    index.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="refresh" content="0;URL=views/index.php" />
+	<title></title>
+</head>
+<body>
+	
+</body>
+</html>
